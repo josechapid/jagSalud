d41be5f6f4ab994cd3b42433c140054c1956fe98
luischapid <luischapid03@gmail.com>
2024-04-29 12:12:45 -0500

    prueba

--- server/src/db.js
+++ server/src/db.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/clinicaHakSalud`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/jagsalud`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
