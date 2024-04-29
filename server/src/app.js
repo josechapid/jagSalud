dbac7778dc1ad2b10b3bced2790e0ce12413b3c8
luischapid <luischapid03@gmail.com>
2024-04-29 12:03:14 -0500

    init

---
+++ server/src/app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
+const cors = require("cors");
+
+require("./db.js");
+
+const server = express();
+
+server.name = "API";
+
+const corsOptions = { origin: "*" };
+server.use(cors(corsOptions));
+
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
+const allowedOrigins = [
+  "http://localhost:5173",
+];
+server.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE, PATCH"
+  );
+  next();
+});
+
+server.use("/", routes);
+
+// Error catching endware.
+
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
