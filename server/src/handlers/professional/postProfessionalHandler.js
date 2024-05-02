a7c0c1e83706d3e4294274efb05ea2a7601e4677
luischapid <luischapid03@gmail.com>
2024-05-02 12:15:35 -0500

    modificaciones

--- server/src/handlers/professional/postProfessionalHandler.js
+++ server/src/handlers/professional/postProfessionalHandler.js
@@ -3,9 +3,9 @@ const postProfessionalController = require('../../controllers/professional/postP
 
 const postProfessionalHandler = async (req, res) => {
   
-  const user = req.body;
+  const professional = req.body;
   try {
-    const response = await postProfessionalController(user);
+    const response = await postProfessionalController(professional);
     res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ error: error.message });
